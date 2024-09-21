cfd17bc576c18dc2622006bd9e6371b7eb14f74b
Erfanmah7 <efn8mah@yahoo.com>
2024-09-21 21:21:59 +0330

    start ghaf

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class="mt-12 mx-auto text-center">
-      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <footer className="mt-12 mx-auto text-center">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           تمامی حقوق این وب سایت متعلق به ghaff.studio می باشد ©
         </span>
       </div>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 function Header() {
   return (
-    <nav class="container transition-all relative  w-full z-20 left-0 right-0 top-0">
-      <div class="flex flex-wrap items-center justify-between mx-auto py-4 px-0">
+    <nav className="container transition-all relative  w-full z-20 left-0 right-0 top-0">
+      <div className="flex flex-wrap items-center justify-between mx-auto py-4 px-0">
         <a
           href="https://flowbite.com/"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <svg className="md:h-[200px] md:w-[200px] w-16 h-16 dark:text-white text-black ">
             <use href="#logo"></use>
           </svg>
         </a>
-        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky"
             aria-expanded="false"
           >
-            <span class="sr-only">Open main menu</span>
+            <span className="sr-only">Open main menu</span>
             <svg
-              class="w-5 h-5"
+              className="w-5 h-5"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -30,23 +30,23 @@ function Header() {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
           </button>
         </div>
         <div
-          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul class="flex text-xl font-IranSansMedium flex-col p-4 md:p-0 mt-4 font-medium md:space-x-24 left-[100px] absolute rtl:space-x-reverse md:flex-row md:mt-0 ">
+          <ul className="flex text-xl font-IranSansMedium flex-col p-4 md:p-0 mt-4 font-medium md:space-x-24 left-[100px] absolute rtl:space-x-reverse md:flex-row md:mt-0 ">
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-white bg-zinc-800 rounded md:bg-transparent md:text-zinc-400 md:p-0 md:dark:text-blue-500"
+                className="block py-2 px-3 text-white bg-zinc-800 rounded md:bg-transparent md:text-zinc-400 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
                 خانه
@@ -55,7 +55,7 @@ function Header() {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 نمونه کار
               </a>
@@ -63,7 +63,7 @@ function Header() {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 ثبت سفارش
               </a>
@@ -71,7 +71,7 @@ function Header() {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 درخواست مشاوره
               </a>
@@ -79,7 +79,7 @@ function Header() {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 مقالات
               </a>
@@ -87,7 +87,7 @@ function Header() {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-zinc-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 درباره ما
               </a>
